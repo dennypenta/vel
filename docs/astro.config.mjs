f8d56b3cf0d429eb81a78b40036e430527e38ab7
Denis <candyboobers@gmail.com>
2025-07-10 22:45:37 +0200

    Draft docs webpage

---
+++ docs/astro.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "vel",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/dennypenta/vel",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ label: "Example Guide", slug: "guides/example" }],
+        },
+        {
+          label: "Tutorials",
+          autogenerate: { directory: "tutorials" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/theme.css"],
+    }),
+  ],
+});
